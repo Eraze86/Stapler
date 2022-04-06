0621beb094e314d9bb6e4b319d0f4efb7eb44a14
Eraze86 <therese.rosvall@hotmail.se>
2022-04-06 14:57:07 +0200

    Form to search avalible time

--- src/components/Booking/booking.tsx
+++ src/components/Booking/booking.tsx
@@ -1,3 +1,22 @@
 export function Booking(){
-    return (<>Booking works!</>)
+    return (<>
+    <form>
+        <label>Datum:</label><br/>
+        <input type="date" ></input><br/>
+        <label>Tid:</label>
+        <select>
+    		<option value="option1">18:00</option>
+    		<option value="option2">21:00</option>
+   		</select><br/>
+           <label>Antal:</label>
+           <select>
+    		<option value="guest1">1</option>
+    		<option value="guest2">2</option>
+            <option value="guest3">3</option>
+            <option value="guest4">4</option>
+            <option value="guest5">5</option>
+            <option value="guest6">6</option>
+   		</select>
+           <button>Sök</button>
+    </form></>)
 }
