c2bfad61be5a6c41252c8354f46a17ced885d13a
bettykroon <betty.kroon@hotmail.com>
2022-04-07 10:22:04 +0200

    Booking class

---
+++ src/modules/Bookings.ts
@@ -0,0 +1,9 @@
+export class Bookings{
+    constructor(
+        public _id: string, 
+        public restaurantId: string, 
+        public date: string, 
+        public time: string, 
+        public numberOfGuests: number, 
+        public customerId: string){}
+}
