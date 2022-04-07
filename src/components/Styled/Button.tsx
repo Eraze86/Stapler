994ae7a8f1fab8fd4266040a232470771a73cb16
lrosenqv <kimle.rosenqvist@gmail.com>
2022-04-07 20:52:05 +0200

    styled bookBtn

---
+++ src/components/Styled/Button.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Button = styled.button `
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap');
+  font-family: 'Raleway', sans-serif;
+  font-weight: 700;
+  font-size: 1.1rem;
+  background-color: #FECB4B;
+  color: white;
+  cursor: pointer;
+  border:none;
+  padding:0.8rem;
+  border-radius: 0.4rem 0.4rem;
+
+  &:hover{
+    background-color: #deaf3c;
+  }
+`;
