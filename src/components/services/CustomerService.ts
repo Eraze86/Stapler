9f2fec876321110a20a5fac0ad1b6636ec93d092
Eraze86 <therese.rosvall@hotmail.se>
2022-04-21 18:57:33 +0200

    documentation

--- src/components/services/CustomerService.ts
+++ src/components/services/CustomerService.ts
@@ -2,6 +2,7 @@ import axios from "axios"
 import { ICustomer } from "../../modules/IBooking"
 
 export class CustomerService{
+  //hämta costumerId
   getCustomer(customerId: string){
       let customer = axios.get<ICustomer[]>("https://school-restaurant-api.azurewebsites.net/customer/" + customerId)
       .then(response => {
